d56b83853a29952c2db7c26d4188cdc5d9893d6e
Julien Lebunetel <julien@lebunetel.com>
2019-06-21 10:36:08 +0200

    Add go to rest position script

---
+++ goto_rest_position.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from niryo_one_python_api.niryo_one_api import *
+import rospy
+rospy.init_node('niryo_one_example_python_api')
+
+n = NiryoOne()
+
+try:
+    n.activate_learning_mode(False)
+    n.move_joints([0,0,0,0,0,0])
+    print "In rest position!"
+
+except NiryoOneException as e:
+    print e
+
+print "--- End"
