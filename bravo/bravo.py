12a0be892d83e4936fd2df11bfab14a744b0390d
Julien Lebunetel <julien@lebunetel.com>
2019-06-21 15:03:35 +0200

    Update bravo.py

--- bravo/bravo.py
+++ bravo/bravo.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, jsonify
+from niryo_one_python_api.niryo_one_api import *
+import time
+import rospy
 
 app = Flask(__name__)
-
-from niryo_one_python_api.niryo_one_api import *
-import rospy
-rospy.init_node('niryo_one_example_python_api')
-
 n = NiryoOne()
-
 
 @app.route("/")
 def index():
@@ -17,6 +14,7 @@ def index():
 def calibrate():
     print "Info: Calibration"
     try:
+        rospy.init_node('niryo_one_example_python_api', disable_signals=True)
         n.calibrate_auto()
         print "Calibration finished !"
         data = {"message":"Calibration"}
@@ -24,7 +22,7 @@ def calibrate():
 
     except NiryoOneException as e:
         print e
-        data = {"message":"Calibration", "error":e}
+        data = {"message":"Calibration", "error":str(e)}
         return jsonify(data)
 
 @app.route("/activate_learning_mode.json")
@@ -32,13 +30,13 @@ def activate_learning_mode():
     print "Info: Activate learning mode"
     try:
         n.activate_learning_mode(True)
-        print "Calibration finished !"
+        print "Activate learning mode"
         data = {"message":"Activate learning mode"}
         return jsonify(data)
 
     except NiryoOneException as e:
         print e
-        data = {"message":"Activate learning mode", "error":e}
+        data = {"message":"Activate learning mode", "error":str(e)}
         return jsonify(data)
 
 @app.route("/deactivate_learning_mode.json")
@@ -46,13 +44,13 @@ def deactivate_learning_mode():
     print "Info: Deactivate learning mode"
     try:
         n.activate_learning_mode(False)
-        print "Calibration finished !"
+        print "Deactivate learning mode"
         data = {"message":"Deactivate learning mode"}
         return jsonify(data)
 
     except NiryoOneException as e:
         print e
-        data = {"message":"Deactivate learning mode", "error":e}
+        data = {"message":"Deactivate learning mode", "error":str(e)}
         return jsonify(data)
 
 @app.route("/goto_rest_position.json")
@@ -62,11 +60,11 @@ def goto_rest_position():
         n.activate_learning_mode(False)
         n.move_joints([0,0.64,-1.397,0,0,0])
         n.activate_learning_mode(True)
-        print "Calibration finished !"
+        print "Goto rest position"
         data = {"message":"Goto rest position"}
         return jsonify(data)
 
     except NiryoOneException as e:
         print e
-        data = {"message":"Goto rest position", "error":e}
+        data = {"message":"Goto rest position", "error":str(e)}
         return jsonify(data)
