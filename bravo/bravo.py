98f217de51519e9c1b3227d12e0110eaae946d7f
Julien Lebunetel <julien@lebunetel.com>
2019-06-24 12:16:06 +0200

    Test position feedback

--- bravo/bravo.py
+++ bravo/bravo.py
@@ -106,7 +106,8 @@ def move():
     print message
     try:
         n.move_joints([joint_1,joint_2,joint_3,joint_4,joint_5,joint_6])
-        data = {"message":message}
+        pos = n.joints
+        data = {"message":message, pos=pos}
         print "[ OK ] finished !"
         return jsonify(data)
 
